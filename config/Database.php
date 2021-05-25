46b54f974b9ba8269d0ef6d7c99c094a8998790d
albinolssonn <56583621+albinolssonn@users.noreply.github.com>
2021-05-25 13:42:20 +0200

    DB Con File

---
+++ config/Database.php
@@ -0,0 +1,23 @@
+<?php
+    class Database{
+        // DB Parameters
+        private $host = 'localhost'; 
+        private $db_name = 'LunicoreDB'; 
+        private $username = 'root'; 
+        private $password = ''
+        private $conn; 
+
+        //DB Connect
+        public function connect(){
+            $this->conn = null; 
+
+            try{
+                $this->conn = new PDO('mysql:host=' . $this->host . ';dbname= ' . $this->db_name, $this->username, $this->password); 
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION) // Sets exceptions when queryhandling goes wrong
+            } catch(PDOExeption $e){
+                echo 'Connection Error: ' . $e->getMessage(); 
+            }
+
+            return $this->conn; 
+        }
+    }
