f0acc25125722226076fb04a0736a57508d34def
albinolssonn <56583621+albinolssonn@users.noreply.github.com>
2021-05-25 17:01:22 +0200

    Git's lagging

--- Models/Cmodel.php
+++ Models/Cmodel.php
@@ -1,31 +1,61 @@
 <?php
+
     class Carmodel {
+
         // For DBconnection
+
         private $conn; 
+
         private $table = 'Carmodels'; 
 
+
+
         // Public Attributes
+
         public $car_id; 
+
         public $brand; 
+
         public $model; 
+
         public $price; 
 
+
+
         // Constructor for DB
+
         public function __construct($db){
+
             $this->conn = $db; 
+
         }
+
+
 
         // Readfunction for Carmodels
+
         public function read(){
+
             //Query
+
             $query = 'SELECT Car_ID, Brand, Model, Price FROM ' . $this->table . ''; 
+
             
+
             //Prep Statement
+
             $stmt = $this->conn->prepare($query); 
 
+
+
             // Execute
+
             $stmt->execute(); 
 
+
+
             return $stmt; 
+
         }
+
     }
--- Models/Emodel.php
+++ Models/Emodel.php
@@ -1,29 +1,58 @@
 <?php
+
     class Employees {
+
         // For DBconnection
+
         private $conn; 
+
         private $table = 'Employees'; 
 
+
+
         // Public Attributes
+
         public $emp_id; 
+
         public $name; 
 
+
+
         // Constructor for DB
+
         public function __construct($db){
+
             $this->conn = $db; 
+
         }
+
+
 
         // Readfunction for Employees
+
         public function read(){
+
             //Query
+
             $query = 'SELECT Emp_ID, Name FROM ' . $this->table . ''; 
+
             
+
             //Prep Statement
+
             $stmt = $this->conn->prepare($query); 
 
+
+
             // Execute
+
             $stmt->execute(); 
 
+
+
             return $stmt; 
+
         }
+
     }
+
