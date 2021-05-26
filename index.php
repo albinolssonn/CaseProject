6c2d5f1623d8d9c707e0bd1b7c9201d2122bc4a7
albinolssonn <56583621+albinolssonn@users.noreply.github.com>
2021-05-26 23:15:02 +0200

    Change file format to php

--- index.php
+++ index.php
@@ -7,17 +7,20 @@
     <link rel="stylesheet" href="/caseproject/CSS/stylesheet.css">
     <link href="https://fonts.googleapis.com/css?family=Quicksand:300,500" rel="stylesheet">
     <script src="/caseproject/JS/main.js"></script>
-    <title>Sales</title>
+    <title>Dashboard</title>
 </head>
 
 
 <body>
+
+    
+    
 <div class="main">
 
     <div class="hero__section">
         <div class="hero__content">
             <div class="hero__title">
-                <h1>Sales</h1>
+                <h1>Welcome to caseproject</h1>
             </div>
         </div>
     </div>
@@ -26,22 +29,40 @@
         <h2>Dashboard</h2>
         <div class="table__grid">
             
-            <div class="table__container" id="sales_table">
+            <div class="table__container" id="emp_table">
                 <h3>Employees</h3>
                 <table class="top-row">
                     <tr>
                         <th>Employee ID</th>
                         <th>Name</th>
-                        <th>Sales</th>
                     </tr>
                 </table>
-                <table id="sales_read">
+                <table id="emp_read">
                     <!-- Here goes Data -->
+                </table>
+            </div>
+
+            <div class="table__container" id="car_table">
+                <h3>Carmodels</h3>
+                <table class="top-row">
+                    <tr>
+                        <th>Car ID</th>
+                        <th>Brand</th>
+                        <th>Model</th>
+                        <th>Price</th>
+                    </tr>
+                </table>
+                <table id="car_read">
+                    <!-- Here goes data -->
                 </table>
             </div>
         </div>      
     </div>
 </div>
+
+<div>
+    test
+</div>    
 
 </body>
 </html>
--- sales.php
+++ sales.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="/caseproject/CSS/stylesheet.css">
     <link href="https://fonts.googleapis.com/css?family=Quicksand:300,500" rel="stylesheet">
     <script src="/caseproject/JS/main.js"></script>
-    <title>Dashboard</title>
+    <title>Sales</title>
 </head>
 
 
@@ -17,45 +17,37 @@
     <div class="hero__section">
         <div class="hero__content">
             <div class="hero__title">
-                <h1>Welcome to caseproject</h1>
+                <h1>Sales</h1>
             </div>
         </div>
     </div>
 
+
+
     <div class="emp__container">
-        <h2>Dashboard</h2>
-        <div class="table__grid">
+        <h2>Sales board</h2>
+        <div class="table__sales">
             
-            <div class="table__container" id="emp_table">
-                <h3>Employees</h3>
+            <div class="table__container__sales" id="sales_table">
+                <h3>Sales per Empoyee</h3>
                 <table class="top-row">
                     <tr>
                         <th>Employee ID</th>
                         <th>Name</th>
+                        <th>Sales</th>
                     </tr>
                 </table>
-                <table id="emp_read">
+                <table id="sales_read">
                     <!-- Here goes Data -->
-                </table>
-            </div>
-
-            <div class="table__container" id="car_table">
-                <h3>Carmodels</h3>
-                <table class="top-row">
-                    <tr>
-                        <th>Car ID</th>
-                        <th>Brand</th>
-                        <th>Model</th>
-                        <th>Price</th>
-                    </tr>
-                </table>
-                <table id="car_read">
-                    <!-- Here goes data -->
                 </table>
             </div>
         </div>      
     </div>
 </div>
+
+<footer>
+    test
+</footer>
 
 </body>
 </html>
