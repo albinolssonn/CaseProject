fa343e567cfd29ded36911a4f11e003956f51c03
albinolssonn <56583621+albinolssonn@users.noreply.github.com>
2021-05-26 14:37:22 +0200

    Html fix

--- HTML/main.js
+++ HTML/main.js
@@ -1,4 +1,5 @@
-function fetchData(){
+// Fetch Method for Carmodel
+function fetchEmpData(){
     fetch("http://localhost:8080/caseproject/API/employees/read.php").then(response =>{
     return response.json(); 
 })
@@ -22,4 +23,33 @@ function fetchData(){
 }); 
 }
 
-fetchData(); 
+// fetchCarData(); 
+
+
+// Fetch Method for Employees
+function fetchCarData(){
+    fetch("http://localhost:8080/caseproject/API/carmodels/read.php").then(response =>{
+    return response.json(); 
+})
+.then(data=>{
+    console.log(data.data); 
+    const html = data.data.map(Carmodels => {
+        return `
+        <div class="Carmodels">
+        <p>Car_ID: ${Carmodels.Car_ID}</p>
+        <p>Brand: ${Carmodels.Brand}</p>
+        <p>Model: ${Carmodels.Model}</p>
+        <p>Price: ${Carmodels.Price}</p>
+        </div>`
+    })
+    .join('');
+    console.log(html); 
+    document
+        .querySelector("#car_read").innerHTML = html; 
+})
+.catch(error =>{
+    console.log(error);
+}); 
+}
+
+// fetchEmpData(); 
